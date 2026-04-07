b632b28b3297f3ea74d2316062872b3a7f6ee7d1
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-04-07 16:19:29 -0400

    feat: added date time help utils

---
+++ apps/web/helper.ts
@@ -0,0 +1,35 @@
+// date time help utilities
+export function formatDateTimeToCustomString(dateTime: string | Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    timeZoneName: "short",
+  };
+
+  const parsedDateTime =
+    dateTime instanceof Date ? dateTime : new Date(dateTime);
+
+  return parsedDateTime.toLocaleString("en-US", options);
+}
+
+export const getSessionDetails = () => {
+  let session = {
+    token: "",
+    user: {},
+  };
+  if (
+    localStorage.getItem("user") !== null &&
+    localStorage.getItem("token") !== null
+  ) {
+    session = {
+      token: localStorage.getItem("token") as string,
+      user: JSON.parse(localStorage.getItem("user") as string),
+    };
+  }
+
+  return session;
+};
