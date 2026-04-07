1defcc1771ce1c7081f3c73c93fe50aca588f317
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-04-07 14:53:01 -0400

    feat: added the sendEmail call

--- packages/email/src/index.ts
+++ packages/email/src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
+import fs from "fs";
+import handlebars from "handlebars";
 
 dotenv.config({
   path: __dirname + "/../.env",
@@ -13,3 +15,26 @@ const transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASSWORD,
   },
 });
+
+export async function sendEmail(
+  toAddr: string,
+  subject: string,
+  body: string,
+  user?: string,
+) {
+  const path = "/templates/" + body;
+  const template = fs.readFileSync(__dirname + path, "utf-8").toString();
+  const html = handlebars.compile(template);
+  const htmlContent = html || "";
+
+  try {
+    await transporter.sendMail({
+      from: process.env.SENDER_EMAIL,
+      toAddr,
+      subject,
+      html: htmlContent,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
