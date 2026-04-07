224dbc7081e301f474699f8a7984d2c3b65428d0
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-04-07 15:56:14 -0400

    feat: upd imports

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import { AuthRouter } from "./AuthRoutes";
+import { TriggerRouter } from "./TriggerRoutes";
+import { ZapRouter } from "./ZapRoutes";
+import { ActionsRouter } from "./ActionsRouter";
 
 const router = express.Router();
 
 router.use("/auth", AuthRouter);
 router.use("/zaps", ZapRouter);
 router.use("/triggers", TriggerRouter);
-router.use("/actions", ActionRouter);
+router.use("/actions", ActionsRouter);
 
 export default router;
